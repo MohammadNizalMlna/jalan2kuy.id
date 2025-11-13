8b620357570785821a8b9d3546f3d20d41920337
MohammadNizalMlna <mohammadnizalmaulana@gmail.com>
2025-11-13 21:22:58 +0700

    update galeri

---
+++ js/galeryData.js
@@ -0,0 +1,8 @@
+const galleryData = [
+  { title: "Ancol, Jakarta", img: "/assets/destinasi/komodo.png" },
+  { title: "Pura Luhur Batukaru, Bali", img: "/assets/destinasi/komodo.png" },
+  { title: "Nusa Penida, Bali", img: "/assets/destinasi/komodo.png" },
+  { title: "Bedugul, Tabanan Bali", img: "/assets/destinasi/komodo.png" },
+  { title: "Pasar Terapung, Muara", img: "/assets/destinasi/komodo.png" },
+  { title: "Bromo, Probolinggo", img: "/assets/destinasi/komodo.png" }
+];
