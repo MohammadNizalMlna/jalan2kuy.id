1355144fa75a2a06e3a1748d921aed8e1a666915
MohammadNizalMlna <mohammadnizalmaulana@gmail.com>
2025-11-26 22:10:40 +0700

    perubahan galeri, jadinya ngambil dari destinasi wisata yang sudah ada

--- js/galeryData.js
+++
@@ -1,8 +0,0 @@
-const galleryData = [
-  { title: "Ancol, Jakarta", img: "/assets/galeri/ancol.jpg" },
-  { title: "Pura Luhur Batukaru, Bali", img: "/assets/galeri/puraluhur.jpg" },
-  { title: "Nusa Penida, Bali", img: "/assets/galeri/nusapenida.jpg" },
-  { title: "Bedugul, Tabanan Bali", img: "/assets/galeri/bedugul.jpg" },
-  { title: "Pasar Terapung, Muara", img: "/assets/galeri/pasarterapung.jpg" },
-  { title: "Bromo, Probolinggo", img: "/assets/galeri/bromo.jpg" }
-];
